dc334d6d84e3ccb322f88718096c169276a268d6
FALLENSLAYER007 <heartrockbreak@gmail.com>
2020-12-24 07:06:41 +0700

    beautify code

--- src/App.js
+++ src/App.js
@@ -18,14 +18,10 @@ import TableRow from '@material-ui/core/TableRow';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
 import PropTypes from 'prop-types';
-
+import {CourseCard} from './components/CourseCard.js';
 
 function App() {
-  const grade = ["A", "B+", "B", "C+", "C", "D+", "D", "F", "W"];
   
-
-  const credit = [1, 2, 3];
- 
 
   const useStyles = makeStyles((theme) => ({
     root: {
@@ -33,32 +29,29 @@ function App() {
         margin: theme.spacing(1),
         width: '25ch',
       },
-      
+
     },
-    form:{
-      
-        maxWidth: 275,
-  
+    form: {
+      maxWidth: 275,
     }
   }));
 
   function SimpleDialog(props) {
     const classes = useStyles();
     const { onClose, selectedValue, open } = props;
-  
+
     const handleClose = () => {
       onClose(selectedValue);
     };
-  
-  
+
     return (
       <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open} >
         <DialogTitle id="simple-dialog-title" color="secondary">{error}</DialogTitle>
-        
+
       </Dialog>
     );
   }
-  
+
   SimpleDialog.propTypes = {
     onClose: PropTypes.func.isRequired,
     open: PropTypes.bool.isRequired,
@@ -66,18 +59,18 @@ function App() {
   };
 
   const classes = useStyles();
-  const [myCourses, setMyCourse] = useState([]);
-  //const [inputData, setInputData] = useState({});
-  const [GPA, setGPA] = useState(0.0);
 
+  const grade = ["A", "B+", "B", "C+", "C", "D+", "D", "F", "W"];
+  const credit = [1, 2, 3];
+
+  const [myCourses, setMyCourse] = useState([]);
+  const [GPA, setGPA] = useState(0.0)
   const [name, setName] = useState("");
   const [subID, setID] = useState("");
   const [gpaSub, setGPAsub] = useState("A");
   const [creDit, setCredit] = useState(1);
   const [error, setError] = useState("");
-
   const [open, setOpen] = useState(false);
-  
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -85,55 +78,52 @@ function App() {
 
   const handleClose = (value) => {
     setOpen(false);
-
   };
+
   /**
    * Calculate the GPA of current courses
    * @returns the GPA of current courses
    */
-  
-
-  function gradeCount(gpa){
-    if(gpa ==="A"){
+  function gradeCount(gpa) {
+    if (gpa === "A") {
       return 4.0
-    }else if(gpa === "B+"){
+    } else if (gpa === "B+") {
       return 3.5
-    }else if(gpa === "B"){
+    } else if (gpa === "B") {
       return 3.0
-    }else if(gpa === "C+"){
+    } else if (gpa === "C+") {
       return 2.5
-    }else if(gpa === "C"){
+    } else if (gpa === "C") {
       return 2.0
-    }else if(gpa === "D+"){
+    } else if (gpa === "D+") {
       return 1.5
-    }else if(gpa === "D"){
+    } else if (gpa === "D") {
       return 1.0
-    }else if(gpa === "F"){
+    } else if (gpa === "F") {
       return 0.0
-    }else if(gpa === "W"){
+    } else if (gpa === "W") {
       return 0.0
     }
   }
 
   function calculateGPA(e) {
     // TODO
-      
-        
-        const courses = [...e]
-        
-            let gpax=0,credit=0
-            courses.forEach(e=> {
-              gpax = gpax+gradeCount(e.gpa)*parseFloat(e.credit)
-              if(e.gpa !== "W")
-                credit = credit + parseFloat(e.credit)
-              console.log(e.gpa)
-              console.log(e.credit)
-            })
-            //setGPA(parseFloat(gpax/credit))
-            if (credit===0) return 0
-            return parseFloat(gpax/credit)
-    
-    
+
+    const courses = [...e]
+
+    let gpax = 0, credit = 0
+    courses.forEach(e => {
+      gpax = gpax + gradeCount(e.gpa) * parseFloat(e.credit)
+      if (e.gpa !== "W")
+        credit = credit + parseFloat(e.credit)
+        console.log(e.gpa)
+        console.log(e.credit)
+    })
+    //setGPA(parseFloat(gpax/credit))
+    if (credit === 0) return 0
+    return parseFloat(gpax / credit)
+
+
   }
 
   /**
@@ -141,24 +131,24 @@ function App() {
    * After adding the course to the list, the displayed GPA should be updated.
    * @param {*} event 
    */
-  
+
   function addCourse(event) {
     event.preventDefault();
     // TODO
-    if(name !== "" && subID !== ""){
-      if(subID.length !==6 ){
-        setError("Subject ID must be 6 digits")
-        handleClickOpen()
-      }else if(isNaN(subID)){
-        setError("Subject ID must be a number")
-        handleClickOpen()
-      }else{
-        
+    if (name !== "" && subID !== "") {
+      if (subID.length !== 6) {
+          setError("Subject ID must be 6 digits")
+          handleClickOpen()
+      } else if (isNaN(subID)) {
+          setError("Subject ID must be a number")
+          handleClickOpen()
+      } else {
+
         const course = [...myCourses]
-        if(course.filter(e=>e.subId === subID).length > 0) {
+        if (course.filter(e => e.subId === subID).length > 0) {
           setError("It already has this subject ID")
           handleClickOpen()
-        }else{
+        } else {
           course.push({
             id: Date.now(),
             name: name,
@@ -166,17 +156,17 @@ function App() {
             credit: creDit,
             subId: subID,
           })
-          
+
           setMyCourse(course)
-          
+
           // recalculate GPA
           setGPA(calculateGPA(course))
         }
       }
-    }else{
-      if(subID === "")
+    } else {
+      if (subID === "")
         setError("Subject ID must be entered")
-      else if(name === "")
+      else if (name === "")
         setError("Subject Name must be entered")
       handleClickOpen()
     }
@@ -199,124 +189,99 @@ function App() {
   }
 
   return (
-    
 
     <div className="container mx-auto h-screen">
-          <Snowfall 
-          snowflakeCount={250}
-          />
+      <Snowfall
+        snowflakeCount={250}
+      />
 
-   
       <h1 class="glow">
         GPA CALCULATOR
       </h1>
- <SimpleDialog open={open} onClose={handleClose} />
+
+      <SimpleDialog open={open} onClose={handleClose} />
+
       <Grid container spacing={3}>
-      <Grid item xs={12} sm={6}>
-      <div align="center">
-        <br/>
-        <Card variant="outlined" className={classes.form}>
-        <Paper elevation={3} />
-        <h1 className="text-center text-4xl p-3 tracking-widest">
-        Add Course
-      </h1>
-      <form className={classes.root} noValidate autoComplete="off" onSubmit={addCourse} width="300px"> 
-      <TextField value={subID} id="outlined-basic" label="Subject ID" variant="outlined" onChange={e => setID(e.target.value)}/>
-      <br></br>
-      <TextField value={name} id="outlined-basic" label="Subject Name" variant="outlined" onChange={e => setName(e.target.value)}/>
-      <InputLabel id="demo-simple-select-label">GPA</InputLabel>
+        <Grid item xs={12} sm={6}>
+          <div align="center">
+            <br />
+            <Card variant="outlined" className={classes.form}>
+              <Paper elevation={3} />
+              <h1 className="text-center text-4xl p-3 tracking-widest">
+                Add Course
+              </h1>
+              <form className={classes.root} noValidate autoComplete="off" onSubmit={addCourse} width="300px">
+                <TextField value={subID} id="outlined-basic" label="Subject ID" variant="outlined" onChange={e => setID(e.target.value)} />
+                <br/><br/>
+                <TextField value={name} id="outlined-basic" label="Subject Name" variant="outlined" onChange={e => setName(e.target.value)} />
+                <InputLabel id="demo-simple-select-label">GPA</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={gpaSub}
+                  onChange={e => { setGPAsub(e.target.value); }}
+                >
+                  {grade.map(e => (
+                    <MenuItem value={e}>{e}</MenuItem>
+                  ))}
+                </Select>
+                <InputLabel id="demo-simple-select-label">Credit</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={creDit}
+                  onChange={e => { setCredit(e.target.value); }}
+                >
+                  {credit.map(e => (
+                    <option value={e}>{e}</option>
+                  ))}
+                </Select>
 
-        <Select
-           labelId="demo-simple-select-label"
-           id="demo-simple-select"
-           value={gpaSub}
-          onChange={e => {setGPAsub(e.target.value);}}
-        > 
-          {grade.map(e =>(
-            <MenuItem value={e}>{e}</MenuItem>
-        ))}
-          
-        </Select>
-        <InputLabel id="demo-simple-select-label">Credit</InputLabel>
-        <Select
-           labelId="demo-simple-select-label"
-           id="demo-simple-select"
-           value={creDit}
-           onChange={e => {setCredit(e.target.value);}}
-           > 
-          {credit.map(e=>(
-            <option value={e}>{e}</option>
-            ))}
-          
-        </Select>
-      
-      <Button variant="contained" color="primary" type="submit">Add</Button>
-      
+                <Button variant="contained" color="primary" type="submit">Add</Button>
+              </form>
+            </Card>
+            <br />
 
-      </form>
-           </Card>
-           <br/>
-      <Card variant="outlined" className={classes.form}>
-        <Paper elevation={3} />
-        <h2 className="text-center text-4xl p-3 tracking-widest">GPA: {GPA}</h2>
-      </Card>
-            </div>
+            <Card variant="outlined" className={classes.form}>
+              <Paper elevation={3} />
+              <h2 className="text-center text-4xl p-3 tracking-widest">GPA: {GPA.toFixed(2)}</h2>
+            </Card>
+          </div>
 
-      
-      
-      
-  </Grid>
-  <Grid item xs={12} sm={6}>
-      <div align="center">
-            
-            
-        <h3 class="small-glow">My courses</h3>
-            
-        <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell >Subject Name</TableCell>
-            <TableCell align="left">Subject ID</TableCell>
-            <TableCell align="left">GPA</TableCell>
-            <TableCell align="left">Credit</TableCell>
-            <TableCell align="left">Delete Button</TableCell>
-            
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        {myCourses.map((value) => {
-        
-        return (
-          <TableRow key={value.id}>
-        
-              <TableCell align="center">{value.name}</TableCell>
-              <TableCell align="center">{value.subId}</TableCell>
-              <TableCell align="center">{value.gpa}</TableCell>
-              <TableCell align="center">{value.credit}</TableCell>
-              <TableCell align="left">
-            <Button
-        variant="contained"
-        color="secondary"
-        onClick={()=>{onDeleteCourse(value.id)}}
-        >
-        Delete
-      </Button>
-      </TableCell>
-          
-          </TableRow>
-        );
-      })}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  
-    </div>
-    </Grid>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <div align="center">
+
+            <h3 class="small-glow">My courses</h3>
+
+            <TableContainer component={Paper}>
+              <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell >Subject Name</TableCell>
+                    <TableCell align="left">Subject ID</TableCell>
+                    <TableCell align="left">GPA</TableCell>
+                    <TableCell align="left">Credit</TableCell>
+                    <TableCell align="left">Delete Button</TableCell>
+
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {myCourses.map((value) => {
+                    return (
+                      CourseCard(value, onDeleteCourse)
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+
+          </div>
+        </Grid>
       </Grid>
-      
+
     </div>
-            
+
   );
 }
 
--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,26 +1,28 @@
 
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
 
-
-export const CourseCard = (props,func) => {
+export const CourseCard = (value,func) => {
   // TODO: design HTML component that displays a course as a "card" on the webpage.
   return (
-    <div>
-      {props.gpa}
-      +
-      {props.id}-
-      {props.name}*
-      {props.credit}/
-      {props.subId}
-      
-      <Button
-        variant="contained"
-        color="secondary"
-        onClick={func}
-      >
-        Delete
-      </Button>
-    </div>
+    <TableRow key={value.id}>
+
+                        <TableCell align="center">{value.name}</TableCell>
+                        <TableCell align="center">{value.subId}</TableCell>
+                        <TableCell align="center">{value.gpa}</TableCell>
+                        <TableCell align="center">{value.credit}</TableCell>
+                        <TableCell align="left">
+                          <Button
+                            variant="contained"
+                            color="secondary"
+                            onClick={() => func(value.id)}
+                          >
+                            Delete
+                          </Button>
+                        </TableCell>
+
+                      </TableRow>
   );
 };
